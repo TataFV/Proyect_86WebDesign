77a64458900a1ac56628a1001a57eb170490e481
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-05-23 10:33:57 +0200

    Creada clase Employee

---
+++ model/Employee.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'user.php';
+
+/**
+* Class Employee
+* Representa a los empleados en la aplicacion
+* Hereda de User
+*/
+class Employee extends User{
+
+    function __construct($name, $lastname, $email, $password){
+        parent::__construct($name, $lastname, $email, $password, 'Employee');
+    }
+
+}
