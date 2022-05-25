5e5f444ecf72d003759c89270d926b06aae3b08a
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-05-25 22:01:44 +0200

    html, contiene formulario de inicio de sesión

---
+++ view/login.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html lang="es">
+    <head>
+    <title></title>
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    </head>
+    <body>
+        <form action="" method="POST">
+            <h2>Iniciar sesión<h2>
+            <p>Nombre de usuario: <br>
+            <input type="text" name="username" id="username"></p>
+            <p>Password: <br>
+            <input type="password" name="password" id="password"></p>
+            <input type="button" id="login" value="Iniciar sesión">
+        </form>
+    </body>
+</html>
