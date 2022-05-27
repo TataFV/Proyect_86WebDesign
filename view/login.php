41017fda504a57e4f33278863602fd8e260279e0
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-05-27 13:05:32 +0200

    Cambio etiquetas, scripts y añadida hoja de estilo

--- view/login.php
+++ view/login.php
@@ -2,41 +2,24 @@
 <html lang="es">
     <head>
     <title></title>
+    <link href="style.css" rel="stylesheet" type="text/css">
     <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     </head>
     <body>
-        <form action="" method="POST">
-            <h2>Iniciar sesión<h2>
-            <p>Nombre de usuario: <br>
-            <input type="text" name="username" id="username"></p>
-            <p>Password: <br>
-            <input type="password" name="password" id="password"></p>
-            <input type="button" id="login" value="Iniciar sesión">
-        </form>
-        <script>
-            var button = document.getElementById("login")
-            button.addEventListener('click', () => {
-                signIn()
-            });
-
-            /**
-            * Inicia sesion
-            * 
-            * Reliza una petición POST 
-            */
-            function signIn(){
-                var email = document.getElementById("username").value;
-                var password = document.getElementById("password").value;
-
-                $.ajax({
-                    data:{"username":email, "password":password},
-                    url:'../controller/LoginController.php',
-                    type:'post',
-                    success:function(response){
-                        console.log(response);
-                    }
-                });
-            }
-        </script>
+        <section id=container>
+            <article>
+                <h2>86WebDesgin</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+                <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+                </svg>
+            </div>
+            <form action="" method="POST">
+                <input type="text" name="username" placeholder="Nombre de usuario" id="username"><br id="brusername">
+                <input type="password" name="password" placeholder="Contraseña"id="password"><br id="brpassword">
+                <input type="button" id="login" value="Iniciar sesión">
+            </form>
+            </article>
+        </section>
+        <script src="login.js"></script>
     </body>
 </html>
