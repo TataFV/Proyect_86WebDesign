6c931f537a181820ab30bbfc07492efaf8427be8
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-05-27 12:27:00 +0200

    Añadido employee a la session

--- controller/loginController.php
+++ controller/loginController.php
@@ -42,23 +42,26 @@ if(is_null($user)){
     * Respuesta que se envía a Ajax
     * @var response
     */
-    $response = ['error' => true, 'response' => 'El usuario no existe']; 
+    $response = ['error' => true, 'type'=>'username', 'response' => 'El usuario no existe']; 
     //Pasa los datos que contiene el array de PHP a JavaScript
     echo json_encode($response);
-    
+
 }else if ($password == $user->get_password()){
         //Crear inicio de sesion
         session_start();
+        $_SESSION['user'] = $user; 
         //si el usuario tiene el rol de CEO 
         if( "CEO" == $user->get_role()){
             //variable_global, almacena datos de inicio de sesion
-
-            $_SESSION['user'] = $user; 
             $response = ['error' => false, 'response' => '../view/ceoPanel.php'];
-            echo json_encode($response);  
+            echo json_encode($response);
+        } else {
+            $response = ['error' => false, 'response' => '../view/employeePanel.php'];
+            echo json_encode($response);
+
         }
 }else{
-        $response = ['error' => true, 'response' => 'La contraseña es incorrecta']; 
+        $response = ['error' => true, 'type'=>'password', 'response' => 'La contraseña es incorrecta']; 
         echo json_encode($response);  
     }
 
