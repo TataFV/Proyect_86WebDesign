975a82a21893514752bb4198b0ec2831eaa2a680
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-06-08 12:35:18 +0200

    Asigna tareas, datos recibidos por el Controller

---
+++ view/resources/scripts/assignTask.js
@@ -0,0 +1,26 @@
+/**
+     * Asigna tareas 
+     * Return {$tasks} - respuesta con los datos 
+     */
+function assignTask() {
+
+    //Hace la peticion de datos al Controller
+    $.ajax({
+        url: '../controller/tasksAssignController.php',
+        method: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+
+}
+assignTask();
+
+function task(data) {
+
+    var employees = data.employees;
+    console.log(employees);
+    var tasks = data.tasks;
+    var series = [];
+}
