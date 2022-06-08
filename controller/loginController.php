e7d36ccf8bbf161c67a4d07dd4d3a76e86182b77
TataFV <92530903+TataFV@users.noreply.github.com>
2022-06-08 20:06:17 +0200

    Feature ceo panel (#27)
    * Creada class Task
    
    * Añadido tipo de tarea
    
    * Cambio variables privadas a públicas
    
    * cambio variables privadas a publicas
    
    * función findFinishedTasks
    
    * task chart controller
    
    * index Ceo Panel
    
    * Mejoras estilo
    
    * Gráfica
    
    * BackEnd creación de tareas y lista de tareas
    
    * Front-End lista de tareas
    
    * Hoja de estilos CSS
    
    * Task list hml
    
    * Background color
    
    * Reorganización de archivos
    
    * Reorganización: resources/css carpeta para hojas de estilo
    
    * Hoja de estilos lista de tareas
    
    * Hoja de estilos: index Ceo Panel
    
    * Hoja de estilos del formulario de tareas
    
    * Hoja de estilos de las tarjetas de empleado
    
    * Reorganización de los html
    
    * Priority valor integer
    
    * Hoja de estilos de la gráfica
    
    * Búsqueda en la db findTaskWithoutEmployee
    
    * Controller de tareas asignadas
    
    * Búsqueda en base de datos de tareas sin asignar
    
    * Asigna tareas, datos recibidos por el Controller
    
    * Reorganización de archivos
    
    * Búsqueda en la db de empleados sin tareas
    
    * Cambios comentarios
    
    * Actualización e introducción de datos en la db
    
    * Función taskCreate
    
    * Controller,  recibe datos del form que crea tareas
    
    * Función taskDelete borra tareas de la db
    
    * Controller del taskDelete
    
    * Crea tabla con lista de tareas y botón de eliminar
    
    * Delete view/scripts directory
    
    * Archivos renombrados
    
    * Hojas de estilo modficadas
    
    * Indentación

--- controller/loginController.php
+++ controller/loginController.php
@@ -6,17 +6,17 @@ require_once '../database/UserQuery.php';
 //Recibe los datos del view y se comunica con la base de datos.
 
 /**
-* El email del usuario con el que inició sesion
-* @acess private
-* @var string
-*/
+ * El email del usuario con el que inició sesion
+ * @acess private
+ * @var string
+ */
 $username = $_POST["username"];
 
 /**
  * La contraseña que el usuario introdujo para iniciar sesion
-* @acess private
-* @var string
-*/
+ * @acess private
+ * @var string
+ */
 $password = $_POST["password"];
 
 /**
@@ -36,32 +36,29 @@ $userDb = new UserQuery();
  */
 $user = $userDb->findUser($username);
 
-if(is_null($user)){ 
+if (is_null($user)) {
 
     /**
-    * Respuesta que se envía a Ajax
-    * @var response
-    */
-    $response = ['error' => true, 'type'=>'username', 'response' => 'El usuario no existe']; 
+     * Respuesta que se envía a Ajax
+     * @var response
+     */
+    $response = ['error' => true, 'type' => 'username', 'response' => 'El usuario no existe'];
     //Pasa los datos que contiene el array de PHP a JavaScript
     echo json_encode($response);
-
-}else if ($password == $user->get_password()){
-        //Crear inicio de sesion
-        session_start();
-        $_SESSION['user'] = $user; 
-        //si el usuario tiene el rol de CEO 
-        if( "CEO" == $user->get_role()){
-            //variable_global, almacena datos de inicio de sesion
-            $response = ['error' => false, 'response' => '../view/ceoPanel.php'];
-            echo json_encode($response);
-        } else {
-            $response = ['error' => false, 'response' => '../view/employeePanel.php'];
-            echo json_encode($response);
-
-        }
-}else{
-        $response = ['error' => true, 'type'=>'password', 'response' => 'La contraseña es incorrecta']; 
-        echo json_encode($response);  
+} else if ($password == $user->get_password()) {
+    //Crear inicio de sesion
+    session_start();
+    $_SESSION['user'] = $user;
+    //si el usuario tiene el rol de CEO 
+    if ("CEO" == $user->get_role()) {
+        //variable_global, almacena datos de inicio de sesion
+        $response = ['error' => false, 'response' => '../view/indexCeoPanel.php'];
+        echo json_encode($response);
+    } else {
+        $response = ['error' => false, 'response' => '../view/employeePanel.php'];
+        echo json_encode($response);
     }
-
+} else {
+    $response = ['error' => true, 'type' => 'password', 'response' => 'La contraseña es incorrecta'];
+    echo json_encode($response);
+}
--- database/UserQuery.php
+++ database/UserQuery.php
@@ -1,15 +1,28 @@
 <?php
 
-require_once ('DBconnection.php');
-require_once ('../model/User.php');
+require_once('DBconnection.php');
+require_once('../model/User.php');
 
 /**
-* Class UserQuery
-* Realiza las consultas del usuario en base de datos
-*/
-Class UserQuery{
+ * Class UserQuery
+ * Realiza las consultas del usuario en base de datos
+ */
+class UserQuery
+{
 
     /**
+
+     * La conexion a la base de datos
+     * @acess private
+     * @var DBconnection
+     */
+    private $db;
+
+    function __construct()
+    {
+        $this->db = new DBconnection();
+    }
+
     * La conexion a la base de datos
     * @acess private
     * @var  DBconnection
@@ -21,25 +34,28 @@ Class UserQuery{
         $this->db=new DBconnection(); 
     }    
 
+
     /**
      * Comprueba si el email del usuario existe en la base de datos y si existe devuelve un usuario
      * @param {string} $email el email del usuario que queremos comprobar 
      * Return {$user} - Null - no existe ningún usuario con ese e-mail 
      */
-    public function findUser($email){
+    public function findUser($email)
+    {
         $sql = "SELECT * FROM user WHERE email='" . $email . "';";
         $result = $this->db->query($sql);
 
-        if($result->num_rows == 0){
+        if ($result->num_rows == 0) {
             return null;
         }
-        
+
         $row = $result->fetch_assoc();
 
         //Instancia en un objeto user la clase User
         $user = new User($row["name"], $row["lastname"], $row["email"], $row["password"], $row["role"]);
         return $user;
     }
+
     public function findEmployees()
     {
         $sql = "SELECT * FROM user WHERE user.role='Employee';";
@@ -80,4 +96,7 @@ Class UserQuery{
 
     }
 }
+
+
+    
 
