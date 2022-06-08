42cef9c72b6e830424cadae729a9d22c78c1cf2a
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-06-08 13:53:21 +0200

    Crea tabla con lista de tareas y botón de eliminar

---
+++ view/resources/scripts/taskList.js
@@ -0,0 +1,56 @@
+
+
+/**
+* Crea una tabla con tareas finalizadas que se pueden borrar 
+* Return {$tasks} - Todas las tareas encontradas
+*/
+function refreshTable() {
+
+
+    $.ajax({
+        url: '../controller/tasksListController.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function (tasks) {
+            let template = '';
+            tasks.forEach(task => {
+                template += `
+                    <tr>
+                        <td class="centeredColumn">${task.id}</td>
+                        <td>${task.name}</td>
+                        <td>${task.description}</td>
+                        <td class="centeredColumn">${task.id_employee}</td>
+                        <td>${task.finishDate}</td>
+                        <td>
+                            <button class="buttonDelete taskDelete" data-task="${task.id}">
+                                ELIMINAR
+                            </button>
+                        </td>
+                    <tr>
+                  `
+            });
+            $('#tasks').html(template);
+            $(document).on('click', '.taskDelete', function () {
+                var id_task = $(this).attr('data-task');
+
+                //Envía al Controller el Id de la tarea que quiere borrar
+                $.ajax({
+                    url: '../controller/taskDeleteController.php',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        "taskId": id_task,
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        refreshTable();
+                    }
+                })
+            });
+
+        }
+    });
+
+}
+
+refreshTable();
