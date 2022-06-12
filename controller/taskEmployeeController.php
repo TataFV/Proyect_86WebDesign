ab14f1670b0e91b936dae25f801a3cbd5f390336
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-06-12 16:00:20 +0200

    Comentarios inicio de sesion

--- controller/taskEmployeeController.php
+++ controller/taskEmployeeController.php
@@ -6,9 +6,13 @@ require_once '../database/TaskQuery.php';
 
 $taskDb = new TaskQuery();
 
+//Inicio de sesion del Empleado
 session_start();
+
+//El usuario es empleado que inicio sesion
 $user = $_SESSION["user"];
 
+//La tarea es la tarea actual  
 $task = $taskDb->findCurrentTask($user->get_id());
 
 echo json_encode($task);
