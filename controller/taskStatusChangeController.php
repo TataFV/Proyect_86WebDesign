0ed496b7673c3d99d4a45193ab6e147b9411db59
mfernandezvillanueva <mfernandezvillanueva@danielcastelao.org>
2022-06-12 18:20:30 +0200

    Comentarios

--- controller/taskStatusChangeController.php
+++ controller/taskStatusChangeController.php
@@ -3,13 +3,17 @@
 require_once '../model/Task.php';
 require_once '../database/TaskQuery.php';
 
+//Guarda el 
 $taskDb = new TaskQuery();
 
 $id = $_POST["id"];
 $status = $_POST["status"];
 
+//Comprobaciones de lo que hacen el botón según el estado de la tarea. 
+//Si el estado de la tarea "es por hacer" se llama a a la función startCurrentTask
 if ($status == "Por hacer"){
     $task = $taskDb->startCurrentTask($id);
+//si el estado es "en curso" se llama a la función finishCurrentTask
 }else{
     $task = $taskDb->finishCurrentTask($id);
 }
